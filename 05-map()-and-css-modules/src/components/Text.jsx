0147b0acd180a31a5531de660578cba218e20e8c
Leandro Comeron <lcomeron@alquilerargentina.com>
2023-02-23 16:10:01 -0300

    fix: delete classname in text component

--- 05-map()-and-css-modules/src/components/Text.jsx
+++ 05-map()-and-css-modules/src/components/Text.jsx
@@ -1,14 +1,12 @@
-import { Fragment } from "react";
 import style from "./Text.module.css";
 
 const Text = (props) => {
   return (
-    <div className={style.container}>
+    <>
       <h1>{props.title}</h1>
       <h2>{props.subTitle}</h2>
       <p>{props.description}</p>
-      {props.children}
-    </div>
+    </>
   );
 };
 
