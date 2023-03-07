7ac97ace54036b463adac7b882624b5d8a449e0d
Leandro Comeron <lcomeron@alquilerargentina.com>
2023-03-07 20:22:44 -0300

    feat: add pre-parcial resolution

--- 07-events-and-forms/src/App.jsx
+++ 07-events-and-forms/src/App.jsx
@@ -5,7 +5,7 @@ import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
-      <h1>Eventos en React ⚛️</h1>
+      {/* <h1>Eventos en React ⚛️</h1> */}
       <Form />
     </div>
   );
--- 07-events-and-forms/src/components/Form.jsx
+++ 07-events-and-forms/src/components/Form.jsx
@@ -5,25 +5,27 @@ function Form() {
   // Seteo todos mis estados y les doy un valor inicial
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [doctor, setDoctor] = useState("");
   const [isErrorName, setIsErrorName] = useState(false);
+  const [showCard, setShowCard] = useState(false);
 
   // creo una variable boolean para mi validación
-  const isValidName = name.length > 10;
+  const isValidName = name.length > 4;
+  const isValidDoctor = !!doctor;
 
   // Función cuando se dispara onSubmit event
   function signUpUser(e) {
     e.preventDefault();
 
-    if (isValidName) {
+    if (isValidName && isValidDoctor) {
       setIsErrorName(false);
       console.log("Usuario guardado");
-      alert(`nombre: ${name} - Password: ${password}`);
+      // alert(`nombre: ${name} - Password: ${password}`);
+      setShowCard(true);
     } else {
       setIsErrorName(true);
       console.log("Tu nombre tiene menos de 10 caracteres");
     }
-    // Reseteo el valor del input name
-    setName("");
   }
 
   // Función cuando se dispara onChange event en el name input
@@ -36,13 +38,17 @@ function Form() {
     setPassword(e.target.value);
   };
 
+  const onChangeDoctor = (e) => {
+    setDoctor(e.target.value);
+  };
+
   return (
     <>
-      <h1>Mi inscripción</h1>
+      <h1>Mi Turno</h1>
       <form onSubmit={signUpUser}>
         <div>
           <input
-            className={!isErrorName ? styles.input : styles.inputError}
+            className={styles.input}
             type="text"
             placeholder="Escriba su nombre"
             value={name}
@@ -53,16 +59,36 @@ function Form() {
         <div>
           <input
             className={styles.input}
-            type="password"
-            placeholder="Escriba su password"
+            type="text"
+            placeholder="Escriba su apellido"
             value={password}
             onChange={onChangePassword}
           />
         </div>
+        <select
+          placeholder="Escoge una médico"
+          onChange={onChangeDoctor}
+          value={doctor}
+        >
+          <option value="" disabled default>
+            Escoge un médico
+          </option>
+
+          <option value="Malavida">Dr. Malavida</option>
+          <option value="Buenavida">Dr. Buenavida</option>
+        </select>
         <div>
           <button type="submit">Guardar</button>
         </div>
       </form>
+      {showCard && (
+        <div className={styles.card}>
+          <div>
+            Gracias por elegirnos {name} {password}
+          </div>
+          <div>Su turno con el doctor {doctor} fue registrado!</div>
+        </div>
+      )}
     </>
   );
 }
