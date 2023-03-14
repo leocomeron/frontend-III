9b92b5a078d5c6e4a20cba1d4384efc51bc222e0
Leandro Comeron <lcomeron@alquilerargentina.com>
2023-03-14 20:20:19 -0300

    feat: class resolution

--- 12-useEffect/src/App.jsx
+++ 12-useEffect/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPedidos([...pedidos, "1 Pizza Fugazzeta"]);
+    }, 2000);
+    return () => clearTimeout();
+  }, [pedidos]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <h1>Ciclo de Vida</h1>
-      <h2>Presionaste el botón {0} veces</h2>
-      <h2>El componente se renderizó: {0} veces</h2>
-      <button onClick={() => {}}>Click aqui</button>
+      <h2>Presionaste el botón {count} veces</h2>
+      {pedidos.map((pedido) => (
+        <li>{pedido}</li>
+      ))}
+      <button onClick={handleClick}>Click aqui</button>
     </div>
   );
 }
