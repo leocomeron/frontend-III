39696880a08a9fd437f5d0790fee6c0465e2e9f0
Leandro Comeron <lcomeron@alquilerargentina.com>
2023-03-15 20:06:55 -0300

    feat: class resolution

--- 13-useEffect-async-calls/src/App.jsx
+++ 13-useEffect-async-calls/src/App.jsx
@@ -1,16 +1,50 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import Card from "./components/Card";
+import Loader from "./components/Loader";
 
 // https://official-joke-api.appspot.com/jokes/programming/random
 
 export default function App() {
-  const [jokes, setJokes] = useState([]);
+  const [bromas, setBromas] = useState([]);
+  const [reload, setReload] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleClick = () => {};
+  const fetchBromas = async () => {
+    setIsLoading(true);
+    const res = await fetch(
+      "https://official-joke-api.appspot.com/jokes/programming/random"
+    );
+    const data = await res.json();
+
+    setBromas(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchBromas();
+  }, [reload]);
+
+  const handleClick = () => {
+    setReload(!reload);
+  };
 
   return (
     <div className="App">
       <h1>Chistes de programación</h1>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        bromas.map((broma) => {
+          return (
+            <Card
+              key={broma.id}
+              setup={broma.setup}
+              punchline={broma.punchline}
+            />
+          );
+        })
+      )}
       <button onClick={handleClick}>Recargar chistes</button>
     </div>
   );
--- 13-useEffect-async-calls/src/components/Card.jsx
+++ 13-useEffect-async-calls/src/components/Card.jsx
@@ -1,3 +1,10 @@
-const Card = () => {};
+const Card = ({ setup, punchline }) => {
+  return (
+    <>
+      <p>{setup}</p>
+      <p>{punchline}</p>
+    </>
+  );
+};
 
 export default Card;
