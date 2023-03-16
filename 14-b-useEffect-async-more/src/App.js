41192d2a71169b674f0f5bed94c2cb0e9d7059b7
Leandro Comeron <lcomeron@alquilerargentina.com>
2023-03-16 12:54:24 -0300

    feat: new changes

---
+++ 14-b-useEffect-async-more/src/App.js
@@ -0,0 +1,143 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function App() {
+  const [courses, setCourses] = useState([]);
+  const [id, setId] = useState("");
+  const [course, setCourse] = useState("");
+  const [teacher, setTeacher] = useState("");
+  const [duration, setDuration] = useState("");
+
+  useEffect(() => {
+    getCourses();
+  }, []);
+
+  async function resetFieldStates() {
+    setId();
+    setCourse("");
+    setDuration("");
+    setTeacher("");
+  }
+
+  async function getCourses() {
+    await axios.get("api/courses").then((response) => {
+      setCourses(response.data.courses);
+    });
+  }
+
+  function fillStates(course) {
+    setId(course.id);
+    setCourse(course.name);
+    setDuration(course.duration);
+    setTeacher(course.teacher);
+  }
+
+  async function addCourse(event) {
+    event.preventDefault();
+    try {
+      await axios.post("api/courses", {
+        name: course,
+        duration,
+        teacher,
+      });
+      resetFieldStates();
+      alert("Curso adicionado correctamente.");
+      getCourses();
+    } catch (error) {
+      alert("Error al adicionar curso." + error.message);
+    }
+  }
+
+  async function editCourse(event) {
+    event.preventDefault();
+    try {
+      await axios.put(`api/courses/${id}`, {
+        name: course,
+        duration,
+        teacher,
+      });
+      resetFieldStates();
+      alert("Curso Editado correctamente.");
+      getCourses();
+    } catch (error) {
+      alert("Error al Editar curso." + error.message);
+    }
+  }
+
+  async function removeCourse(id) {
+    try {
+      await axios.delete(`api/courses/${id}`);
+      alert("Curso Eliminado correctamente.");
+      getCourses();
+    } catch (error) {
+      alert("Error al Eliminar curso." + error.message);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Cursos</h1>
+      <form
+        onSubmit={id ? editCourse : addCourse}
+        style={{
+          marginBottom: 20,
+          display: "flex",
+          gap: 10,
+        }}
+      >
+        <input
+          placeholder="Curso"
+          value={course}
+          onChange={(event) => setCourse(event.target.value)}
+        />
+        <input
+          placeholder="Carga horaria"
+          value={duration}
+          onChange={(event) => setDuration(event.target.value)}
+        />
+        <input
+          placeholder="Professor"
+          value={teacher}
+          onChange={(event) => setTeacher(event.target.value)}
+        />
+        <input type="submit" />
+      </form>
+      <table border="1" style={{ width: 600 }}>
+        <thead>
+          <tr>
+            <td style={{ background: "#cacaca" }} width="35%">
+              Nombre
+            </td>
+            <td style={{ background: "#cacaca" }} width="10%">
+              Carga horaria
+            </td>
+            <td style={{ background: "#cacaca" }} width="30%">
+              Nombre
+            </td>
+            <td style={{ background: "#cacaca" }} width="25%">
+              Acciones
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr key={course.id}>
+              <td>{course.name}</td>
+              <td>{course.duration}</td>
+              <td>{course.teacher}</td>
+              <td>
+                <button onClick={() => removeCourse(course.id)}>
+                  Eliminar
+                </button>
+                <button onClick={() => fillStates(course)}>Editar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ 14-b-useEffect-async-more/src/index.js
@@ -0,0 +1,66 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createServer, Model } from "miragejs";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+createServer({
+  models: {
+    course: Model,
+  },
+  seeds(server) {
+    server.create("course", {
+      name: "Desenvolvimento Frontend",
+      duration: 80,
+      teacher: "Wesley",
+    });
+    server.create("course", {
+      name: "Desenvolvimento Mobile",
+      duration: 100,
+      teacher: "Bruno",
+    });
+    server.create("course", {
+      name: "Desenvolvimento FullStack",
+      duration: 80,
+      teacher: "Ana",
+    });
+  },
+  routes() {
+    this.namespace = "api/courses";
+
+    this.get("/", (schema, request) => {
+      return schema.courses.all();
+    });
+
+    this.get("/:id", (schema, request) => {
+      let id = request.params.id;
+      return schema.courses.find(id);
+    });
+
+    this.put("/:id", (schema, request) => {
+      let newAttrs = JSON.parse(request.requestBody);
+      let id = request.params.id;
+      let course = schema.courses.find(id);
+      return course.update(newAttrs);
+    });
+
+    this.post("/", (schema, request) => {
+      let attrs = JSON.parse(request.requestBody);
+      return schema.courses.create(attrs);
+    });
+
+    this.delete("/:id", (schema, request) => {
+      let id = request.params.id;
+      return schema.courses.find(id).destroy();
+    });
+  },
+});
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
